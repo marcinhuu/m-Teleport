67d2e28d8a7475d8e448720c1452d1e6c6515ecf
marcinhu <marcio_gomes11@hotmail.com>
2022-06-10 18:03:05 +0100

    + Add option to citizenID

--- configs/config.lua
+++ configs/config.lua
@@ -6,11 +6,15 @@ Config = {}
 Config.Utility = {
     CoreName = "qb-core", -- Core Name
     KeyTeleport = 38,
+    Check = "license", -- "license" or "citizenid"
 }
 
 Config.Licenses = { -- Your licenses
     { license = "license:XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"},
-    -- You can add more 
+}
+
+Config.CitizenID = { -- Your citizenid
+    { citizenid = "ASDASDASD"},
 }
 
 Config.Teleports = {
@@ -18,12 +22,12 @@ Config.Teleports = {
         [1] = { -- Go In
             coords = vector4(-143.94, -576.18, 32.42, 338.01),
             AllowVehicle = true,
-            drawText = '[E] - Go in!'
+            drawText = '[E] - Go in bitches!'
         },
         [2] = { -- Go Out
             coords = vector4(-196.23, -579.94, 135.58, 279.06),
             AllowVehicle = true,
-            drawText = '[E] Go out!'
+            drawText = '[E] Go out bitches!'
         },
     },
     --[[ -- Add more teleports
@@ -31,12 +35,12 @@ Config.Teleports = {
         [1] = { -- Go In
             coords = vector4(-143.94, -576.18, 32.42, 338.01),
             AllowVehicle = true,
-            drawText = '[E] - Go in!'
+            drawText = '[E] - Go in bitches!'
         },
         [2] = { -- Go Out
             coords = vector4(-196.23, -579.94, 135.58, 279.06),
             AllowVehicle = true,
-            drawText = '[E] Go out!'
+            drawText = '[E] Go out bitches!'
         },
     },
     ]]
--- server/server.lua
+++ server/server.lua
@@ -3,12 +3,24 @@ local QBCore = exports[Config.Utility.CoreName]:GetCoreObject()
 QBCore.Functions.CreateCallback("m-Teleport:Server:Pode", function(source, cb)
     local src = source
     local license = QBCore.Functions.GetIdentifier(src, 'license')
-    for i, v in pairs(Config.Licenses) do
-        if v.license == license then
-            cb(true)
-        else 
-            TriggerClientEvent('m-Teleport:Client:Notify', src, "You can't do this.")
-            cb(false)
+    local citizenid = QBCore.Functions.GetIdentifier(src, 'citizenid')
+    if Config.Utility.Check == "license" then
+        for i, v in pairs(Config.Licenses) do
+            if v.license == license then
+                cb(true)
+            else 
+                TriggerClientEvent('m-Teleport:Client:Notify', src, "You can't do this.")
+                cb(false)
+            end
+        end
+    elseif Config.Utility.Check == "license" then
+        for i, v in pairs(Config.CitizenID) do
+            if v.citizenid == citizenid then
+                cb(true)
+            else 
+                TriggerClientEvent('m-Teleport:Client:Notify', src, "You can't do this.")
+                cb(false)
+            end
         end
     end
 end)
